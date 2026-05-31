91c9f984b1c198a4de1103821d25d1ca6c8f869e
John Rheyver Gatchalian <johnrheyver.gatchalian@cvsu.edu.ph>
2026-05-31 19:28:53 +0800

    Update db_connect.php

--- submit/db_connect.php
+++ submit/db_connect.php
@@ -1,1 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";   // default for XAMPP
+$password = "";       // default for XAMPP
+$dbname = "message_app_db"; // must match your database name
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("❌ Connection failed: " . $conn->connect_error);
+}
+?>
 

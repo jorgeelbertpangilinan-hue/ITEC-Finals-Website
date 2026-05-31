9c7323a7c6c0344848807ddede3d7ce1e69d4b5d
Zylo Vilarde <145177617+Zzzylo@users.noreply.github.com>
2026-05-31 19:34:52 +0800

    Update message_form.php

--- submit/message_form.php
+++ submit/message_form.php
@@ -2,8 +2,12 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@400;500;600;700&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="common.css">
+    <link rel="stylesheet" href="submit.css">
   <title>Freedom Wall</title>
-  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <nav>

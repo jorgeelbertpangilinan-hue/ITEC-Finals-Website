e56f405c750845426c4260ca5b148d6ed115465c
kianmatthewdimayuga-crypto <kianmatthew.dimayuga@cvsu.edu.ph>
2026-06-03 16:02:32 +0800

    Update connect.php

--- log-in/connect.php
+++ log-in/connect.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "itecyansha";
 
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Database Connection Failed: " . $conn->connect_error);
+}
+?>
